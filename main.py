706088db220d38bc3702bf3a2b4a8d0f8b326292
zaydenSnacks <57055064+zaydenSnacks@users.noreply.github.com>
2020-11-05 21:27:36 -0500

    pagination

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from flask import Flask, redirect, url_for, render_template, request
 import requests
 from datetime import timedelta, date
 
-newsapi = NewsApiClient(api_key='579c378a51e64237a1a1ad13d9069103')
+newsapi = NewsApiClient(api_key='78b9d599c4f94f8fa3afb1a5458928d6')
 
 
 app = Flask(__name__)
 @app.route("/")
 def home():
-    all_articles = newsapi.get_everything(q="technology OR entertainment OR sports", qintitle="technology OR entertainment OR sports", language="en", sort_by="relevancy",page_size=40, from_param=date.today()-timedelta(days=1), to=date.today())
+    all_articles = newsapi.get_everything(q="technology OR entertainment OR sports", qintitle="technology OR entertainment OR sports", language="en", sort_by="relevancy",page_size=50, from_param=date.today()-timedelta(days=1), to=date.today())
     return render_template("index.html", articles_list=all_articles['articles'])
 
 
@@ -29,15 +29,15 @@ def technology():
     response = newsapi.get_top_headlines(category="technology", country="us", language="en", page_size=50)
     return render_template("index.html", articles_list=response['articles'])
 
-@app.route("/search/", methods=["POST", "GET"])
-def search():
+@app.route("/search/<int:page_number>", methods=["POST", "GET"])
+def search(page_number=1):
 
-    if request.form["nm"] == "":
+    if request.form["nm"].lower() == "":
         return home()
 
-    response = newsapi.get_everything(q= "technology OR entertainment OR sports AND " + request.form["nm"] + " NOT politics",
-                                          qintitle="technology OR entertainment OR sports AND " + request.form["nm"], language="en",
-                                          sort_by="relevancy", page_size=40,
+    response = newsapi.get_everything(q= "(technology OR entertainment OR sports) AND " + request.form["nm"] + " NOT politics",
+                                          qintitle="(technology OR entertainment OR sports) AND " + request.form["nm"], language="en",
+                                          sort_by="relevancy", page_size=50, page= 1,
                                           from_param=date.today() - timedelta(days=1), to=date.today())
     if response['totalResults'] == 0:
         return render_template("zero_results.html")
--- main2.py
+++ main2.py
@@ -5,8 +5,12 @@ from datetime import timedelta, date
 
 newsapi = NewsApiClient(api_key='579c378a51e64237a1a1ad13d9069103')
 
-all_articles = newsapi.get_everything(q="technology OR entertainment OR sports AND bitcoin", qintitle="technology OR entertainment OR sports AND bitcoin", language="en", sort_by="relevancy",page_size=40, from_param=date.today()-timedelta(days=1), to=date.today())
-
+hashSet = set()
+all_articles = newsapi.get_sources(category="technology", country="us", language="en") #newsapi.get_everything(q="technology OR entertainment OR sports", qintitle="technology OR entertainment OR sports AND bitcoin", language="en", sort_by="relevancy",page_size=40, from_param=date.today()-timedelta(days=1), to=date.today())
 
 
 print(all_articles)
+# for i in range(len(all_articles['articles'])):
+#     hashSet.add(all_articles['articles'][i]['source']['name'])
+#
+# print(len(hashSet))
