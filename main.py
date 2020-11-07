352f3a17f5a40b0052a9d929d7ac23c5c7b0dc93
zaydenSnacks <57055064+zaydenSnacks@users.noreply.github.com>
2020-11-06 21:03:31 -0500

    initalization

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ newsapi = NewsApiClient(api_key='78b9d599c4f94f8fa3afb1a5458928d6')
 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/")  # home page
 def home():
     all_articles = newsapi.get_everything(q="technology OR entertainment OR sports", qintitle="technology OR entertainment OR sports", language="en", sort_by="relevancy",page_size=50, from_param=date.today()-timedelta(days=1), to=date.today())
     return render_template("index.html", articles_list=all_articles['articles'])
 
 
 
-@app.route("/sports/")
+@app.route("/sports/")  #sports page
 def sports():
     response = newsapi.get_top_headlines(category="sports", country="us", language="en", page_size=50)
     return render_template("index.html", articles_list=response['articles'])
 
-@app.route("/entertainment/")
+@app.route("/entertainment/") #entertainment page
 def entertainment():
     response = newsapi.get_top_headlines(category="entertainment", country="us", language="en", page_size=50)
     return render_template("index.html", articles_list=response['articles'])
 
-@app.route("/technology/")
+@app.route("/technology/")  #technology page
 def technology():
     response = newsapi.get_top_headlines(category="technology", country="us", language="en", page_size=50)
     return render_template("index.html", articles_list=response['articles'])
@@ -32,14 +32,14 @@ def technology():
 @app.route("/search/", methods=["POST", "GET"])
 def search():
 
-    if request.form["nm"].lower() == "":
+    if request.form["nm"].lower() == "": # taking user input from search bar
         return home()
 
     response = newsapi.get_everything(q= "technology OR entertainment OR sports AND " + request.form["nm"],
                                           qintitle="technology OR entertainment OR sports AND " + request.form["nm"], language="en",
                                           sort_by="relevancy", page_size=50,
                                           from_param=date.today() - timedelta(days=1), to=date.today())
-    if response['totalResults'] == 0 or response['status'] != 'ok':
+    if response['totalResults'] == 0 or response['status'] != 'ok':     # checking that search results are valid
         return render_template("zero_results.html")
 
     return render_template("index.html", articles_list=response['articles'])
