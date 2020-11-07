6673c7e9931e8c28e33541d64bf73a86bf89177b
zaydenSnacks <57055064+zaydenSnacks@users.noreply.github.com>
2020-11-06 22:00:50 -0500

    comments

--- main.py
+++ main.py
@@ -7,39 +7,39 @@ newsapi = NewsApiClient(api_key='78b9d599c4f94f8fa3afb1a5458928d6')
 
 
 app = Flask(__name__)
-@app.route("/")  # home page
+@app.route("/")                                                                                                    # home page
 def home():
     all_articles = newsapi.get_everything(q="technology OR entertainment OR sports", qintitle="technology OR entertainment OR sports", language="en", sort_by="relevancy",page_size=50, from_param=date.today()-timedelta(days=1), to=date.today())
-    return render_template("index.html", articles_list=all_articles['articles'])
+    return render_template("index.html", articles_list=all_articles['articles'])                                       # newsApi filters everything that is not technology, sports or entertainment
 
 
 
-@app.route("/sports/")  #sports page
+@app.route("/sports/")                                                                                              #sports page
 def sports():
     response = newsapi.get_top_headlines(category="sports", country="us", language="en", page_size=50)
     return render_template("index.html", articles_list=response['articles'])
 
-@app.route("/entertainment/") #entertainment page
+@app.route("/entertainment/")                                                                                   #entertainment page
 def entertainment():
     response = newsapi.get_top_headlines(category="entertainment", country="us", language="en", page_size=50)
     return render_template("index.html", articles_list=response['articles'])
 
-@app.route("/technology/")  #technology page
+@app.route("/technology/")                                                                                          #technology page
 def technology():
     response = newsapi.get_top_headlines(category="technology", country="us", language="en", page_size=50)
     return render_template("index.html", articles_list=response['articles'])
 
-@app.route("/search/", methods=["POST", "GET"])
+@app.route("/search/")                                                                              # Only vists this page if the search but has been pressed
 def search():
 
-    if request.form["nm"].lower() == "": # taking user input from search bar
+    if request.form["nm"].lower() == "":                                                                                                    # taking user input from search bar
         return home()
 
-    response = newsapi.get_everything(q= "technology OR entertainment OR sports AND " + request.form["nm"],
+    response = newsapi.get_everything(q= "technology OR entertainment OR sports AND " + request.form["nm"],                                 # filtering technology entertainment and sports with user input
                                           qintitle="technology OR entertainment OR sports AND " + request.form["nm"], language="en",
                                           sort_by="relevancy", page_size=50,
                                           from_param=date.today() - timedelta(days=1), to=date.today())
-    if response['totalResults'] == 0 or response['status'] != 'ok':     # checking that search results are valid
+    if response['totalResults'] == 0 or response['status'] != 'ok':                                                                        # checking that search results are valid
         return render_template("zero_results.html")
 
     return render_template("index.html", articles_list=response['articles'])
